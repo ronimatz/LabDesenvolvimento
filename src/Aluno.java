627c03542fe009660aa4e1ce0e40a320f1740be1
Rafael de Paiva <64436487+RafaelDePaiva@users.noreply.github.com>
2025-02-25 15:59:55 -0300

    Update Aluno.java

---
+++ src/Aluno.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Aluno extends User{
+    private String idAluno;
+    private int numMatricula;
+    private Arraylist <Disciplina>  disciplinasMatriculadas[];
+
+
+
+
+
+    public boolean matricular(Disciplina d){
+
+    }
+
+
+    public boolean cancelarMatricula(Disciplina d){
+
+    }
+
+    public int verificaQtdObg(){
+
+    }
+}
