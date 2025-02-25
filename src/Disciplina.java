0eafdc1483388086f9165a1a1c6d85f6add8030d
Rafael de Paiva <64436487+RafaelDePaiva@users.noreply.github.com>
2025-02-25 16:00:13 -0300

    Update Disciplina.java

---
+++ src/Disciplina.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Disciplina {
+    private String nome;
+    private boolean obrigatoria;
+    private boolean ativa;
+    private final int MAX_ALUNOS = 60;
+    private ArrayList<Aluno> alunosMatriculados[];
+
+
+
+
+    public int getQtdAlunos(){
+
+    }
+
+    public boolean desativaDisciplina(){
+
+    }
+
+}
