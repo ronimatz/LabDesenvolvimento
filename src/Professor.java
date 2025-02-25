0e11cac05120b8530e6aa1b61c018de41597dd20
Rafael de Paiva <64436487+RafaelDePaiva@users.noreply.github.com>
2025-02-25 16:00:33 -0300

    Update Professor.java

---
+++ src/Professor.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Professor extends User {
+    private String idProfessor;
+
+
+    public ArrayList<Aluno> verificaAlunos (Disciplina d){
+return null;
+    }
+}
