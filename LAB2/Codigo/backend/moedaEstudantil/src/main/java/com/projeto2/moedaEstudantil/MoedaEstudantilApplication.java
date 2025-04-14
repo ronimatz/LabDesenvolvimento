62e75b1ef281fd70c97bb3476a7fbcd2b48ccc6d
CaioVKodato <caiovictorkodatot@gmail.com>
2025-04-14 15:42:09 -0300

    Projeto spring inicializado

---
+++ LAB2/Codigo/backend/moedaEstudantil/src/main/java/com/projeto2/moedaEstudantil/MoedaEstudantilApplication.java
@@ -0,0 +1,13 @@
+package com.projeto2.moedaEstudantil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoedaEstudantilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoedaEstudantilApplication.class, args);
+	}
+
+}
