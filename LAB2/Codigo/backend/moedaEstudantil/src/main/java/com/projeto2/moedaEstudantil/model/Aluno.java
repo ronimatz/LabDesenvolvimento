edce5cac25369445dc800773147e3a0702746336
CaioVKodato <caiovictorkodatot@gmail.com>
2025-04-19 09:50:13 -0300

    Modificações classe aluno

--- LAB2/Codigo/backend/moedaEstudantil/src/main/java/com/projeto2/moedaEstudantil/model/Aluno.java
+++ LAB2/Codigo/backend/moedaEstudantil/src/main/java/com/projeto2/moedaEstudantil/model/Aluno.java
@@ -11,12 +11,14 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "alunos")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Aluno extends Usuario {
     @Column(nullable = false)
     private String nome;
