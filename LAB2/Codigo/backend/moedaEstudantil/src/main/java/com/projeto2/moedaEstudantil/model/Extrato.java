3ae93ed2f3bebe87e7eb59a68cb0fad3f63a57dc
CaioVKodato <caiovictorkodatot@gmail.com>
2025-04-19 09:51:02 -0300

    Model do extrato

--- LAB2/Codigo/backend/moedaEstudantil/src/main/java/com/projeto2/moedaEstudantil/model/Extrato.java
+++ LAB2/Codigo/backend/moedaEstudantil/src/main/java/com/projeto2/moedaEstudantil/model/Extrato.java
@@ -1,5 +1,32 @@
 package com.projeto2.moedaEstudantil.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "extrato")
+@Getter
+@Setter
 public class Extrato {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    private Integer saldoMoedas;
+
+    @OneToOne
+    private Usuario usuario;
+
+    @OneToMany
+    private List<Transacoes> transacoes;
 }
